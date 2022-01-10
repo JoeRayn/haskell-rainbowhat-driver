ff773c743a173d24a10a43198a57d975931415a6
Joe Rayner <tombstoneman27@gmail.com>
2022-01-11 00:06:37 +0100

    Import the SPI pins, but trying to work out the right pins raised more questions than it answered

--- src/Apa102.hs
+++ src/Apa102.hs
@@ -8,16 +8,22 @@ import qualified Data.ByteString as BS
 import Data.Word
 
 
-runApa102 = withGPIO . withSPI $ do
-  chipSelectSPI
+-- ToDo read about the SPI https://learn.sparkfun.com/tutorials/serial-peripheral-interface-spi
+-- the python seems to actually treat the them a normal GPIO pins and does not actually use the spi protocal i.e. manually pusling the clock. I dont think that is what real SPI should be however need to read more
 
-  
--- import System.Hardware.WiringPi
--- import Data.List
+{- corisponds to gpio pin 8 -}
+chipSelectPin = CS0
 
--- dat = 10
--- clk = 11
--- cs = 8
+{- trasfer 1 byte on MOSI pin. This seem to be the one used in the python libary, corrisponds to GPIO pin 10 in python library -}
+trasferMOSI = transferSPI
+
+-- I think we either need these GPIO pins or use the SPI function and pins but not both, in any case these value names now match (round abouts) the python library 
+clockPin = Pin23
+chipSelectPinGpio = Pin24
+dataPin = Pin19
+
+
+
 -- numPixels = 7
 -- brightness = 7
 
@@ -27,3 +33,7 @@ runApa102 = withGPIO . withSPI $ do
 
 -- clear :: Pixels -> Pixels
 -- clear (_, _, _, brightness) = (0, 0, 0, brightness)
+
+runApa102 = withGPIO . withSPI $ do
+  chipSelectSPI chipSelectPin
+
