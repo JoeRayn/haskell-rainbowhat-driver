aebf262dcc50595f6883004175f041dcf73dfd09
Joe Rayner <tombstoneman27@gmail.com>
2022-03-13 00:39:10 +0100

    clean up cabal file

--- src/Main.hs
+++ src/Main.hs
@@ -5,7 +5,10 @@ import           AlphaNumDisplay
 import           Apa102
 import           Bmp280
 
-
+-- once everything is working I think Im going to need to use some concurrency here to get
+-- everything to run at the same time, good thing I watched that lecture series
 main :: IO ()
 main = do
+   runApa102
    bmp_setup
+   -- runClock 
